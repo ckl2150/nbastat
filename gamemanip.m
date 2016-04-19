138504e5ef3ac4e37169bc2b5b89ce1e0a741a5c
ckl2150 <conrad.liu21@gmail.com>
2016-04-18 22:06:09 -0400

    added away/home formats to lastngames, and began central function

---
+++ gamemanip.m
@@ -0,0 +1,8 @@
+function structure = gamemanip(games,str)
+%Central function used to call specific subfunctions as needed, to
+%manipulate vector of games data into desired format
+
+
+
+end
+
--- lastngames.m
+++ lastngames.m
@@ -1,6 +1,12 @@
-function avgs = lastngames(games, n)
+function avgs = lastngames(games, n, varargin)
 %function which takes a vector of structures as input and a number n, and
 %returns the average numbers of all categories over n games.
+
+if nargin == 3
+    format = varargin{1};
+else
+    format = '';
+end
 
 %counter to track number of games played
 count = 1;
@@ -28,26 +34,43 @@ while count ~= n+1
     %only retrieves information if player has played in the current game
     if ~isempty(games(gamenum).G)
         
-        %Converts minutes to decimal format
-        [min, sec] = strtok(games(gamenum).MP,':');
-        sec = sec(2:3);
-        totaltime = str2double(sec)/60 + str2double(min);
-        minutes(count) = totaltime;
+        %the constraints below ensure that if home/away games are desired,
+        %games not in that format change a boolean such that it skips the
+        %current game
         
-        fgmade(count) = str2double(games(gamenum).FG);
-        fgatmpt(count) = str2double(games(gamenum).FGA);
-        threeptmade(count) = str2double(games(gamenum).Threept);
-        threeptatmpt(count) = str2double(games(gamenum).ThreePtAmpt);
-        ftmade(count) = str2double(games(gamenum).FT);
-        ftatmpt(count) = str2double(games(gamenum).FTA);
-        rebounds(count) = str2double(games(gamenum).TRB);
-        assists(count) = str2double(games(gamenum).AST);
-        steals(count) = str2double(games(gamenum).STL);
-        blocks(count) = str2double(games(gamenum).BLK);
-        to(count) = str2double(games(gamenum).TOV);
-        pts(count) = str2double(games(gamenum).PTS);
-        count = count + 1;
-        gamenum = gamenum - 1;
+        pass = false;
+        
+        if strcmp(format,'away') && isempty(games(gamenum).At)
+            pass = true;
+        elseif strcmp(format,'home') && ~isempty(games(gamenum).At)
+            pass = true;
+        end
+        
+        if ~pass
+            disp(pass)
+            %Converts minutes to decimal format
+            [min, sec] = strtok(games(gamenum).MP,':');
+            sec = sec(2:3);
+            totaltime = str2double(sec)/60 + str2double(min);
+            minutes(count) = totaltime;
+        
+            fgmade(count) = str2double(games(gamenum).FG);
+            fgatmpt(count) = str2double(games(gamenum).FGA);
+            threeptmade(count) = str2double(games(gamenum).Threept);
+            threeptatmpt(count) = str2double(games(gamenum).ThreePtAmpt);
+            ftmade(count) = str2double(games(gamenum).FT);
+            ftatmpt(count) = str2double(games(gamenum).FTA);
+            rebounds(count) = str2double(games(gamenum).TRB);
+            assists(count) = str2double(games(gamenum).AST);
+            steals(count) = str2double(games(gamenum).STL);
+            blocks(count) = str2double(games(gamenum).BLK);
+            to(count) = str2double(games(gamenum).TOV);
+            pts(count) = str2double(games(gamenum).PTS);
+            count = count + 1;
+            gamenum = gamenum - 1;
+        else
+            gamenum = gamenum - 1;
+        end
     else
         gamenum = gamenum - 1;
     end
@@ -70,7 +93,7 @@ blocks = mean(blocks);
 to = mean(to);
 pts = mean(pts);
 
-avgs = struct('TP', minutes, 'FGp', fgp, 'ThreePtp', threepp, 'FTp', ftp, 'Rebounds',...
+avgs = struct('MP', minutes, 'FGp', fgp, 'ThreePtp', threepp, 'FTp', ftp, 'Rebounds',...
     rebounds, 'Assists', assists, 'Steals', steals, 'Blocks', blocks,...
     'TO', to, 'Pts', pts);
 
