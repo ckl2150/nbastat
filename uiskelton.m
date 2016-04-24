2e63a6aa92282cfd151d54e100d1166f0049d4a2
ckl2150 <conrad.liu21@gmail.com>
2016-04-24 18:08:44 -0400

    merged mine and jeff's versions

--- uiskelton.m
+++ uiskelton.m
@@ -3,6 +3,8 @@
 %search for players, a figure to show a single player's stats and a figure
 %to compare two players.
 
+
+
 function uiskelton
 
 players = parsePlayer();
@@ -13,10 +15,10 @@ currentplayer2 = struct('team', '', 'posit', '', 'first', '', 'last', '',...
     'filename', '', 'fullname', '');
 
 %First, creates opening screen ui
-openf = figure('Visible','off','color','white',...
-    'Position',[300,500,1200,800]);
+openf = figure('Visible','off','color','white','Units','Normalized',...
+    'Position',[0,0,1,1]);
 set(openf,'Name','Welcome to Our Awesome Project')
-movegui(openf,'center')
+movegui(openf,'center') %Can now probably get rid of this
 
 logo = imread('nba-logo-on-wood.jpg');
 nbalogo = image(logo);
@@ -26,37 +28,40 @@ set(gca,'Visible','off')
 %stats path or compare two players path
 
 %Create button group for path options
-optionpbg = uibuttongroup('Visible','off','Position',[0 0 .25 1],...
-    'backgroundcolor','white','BorderType','none');
+optionpbg = uibuttongroup('Visible','off','Units','Normalized',...
+    'Position',[0 0 .2 1],'backgroundcolor','white','BorderType','none');
 
 %Textbox with instructions on which button to push
 searchinstruct = uicontrol(optionpbg,'Style','text',...
-    'BackgroundColor','white','Position',[01 425 200 30],...
-    'String','Would you like to:','FontSize',18);
+    'BackgroundColor','white','Units','Normalized',...
+    'Position',[0 .45 .8 .1],'String','Would you like to:','FontSize',18);
 
 %Pushbutton for looking at single player
 singleplayer = uicontrol(optionpbg,'Style','pushbutton',...
-    'String','Look at single player','Position',[01 350 200 30],...
-    'Callback',@spchosen);%callback function to open search box
+    'String','Look at single player','Units','Normalized',...
+    'Position',[.1 .4 .75 .075],'Callback',@spchosen);%callback function to open search box
 %Pushbutton for comparing two players
 twoplayers = uicontrol(optionpbg,'Style','pushbutton',...
-    'String','Compare two players','Position',[01 250 200 30],...
-    'Callback',@tpchosen);%callback function to open player-1 search box
+    'String','Compare two players','Units','Normalized',...
+    'Position',[.1 .3 .75 .075],'Callback',@tpchosen);%callback function to open player-1 search box
 
 %Create figure to show single player stats
 singleplayerfig = figure('Visible','off','color','white',...
-    'Position',[300,500,1200,800]);
+    'Units','Normalized','Position',[0 0 1 1]);
 set(singleplayerfig,'Name','Welcome to Our Awesome Project')
 movegui(singleplayerfig,'center')
 
 
 %Create figure to show two player comparison
 compare2fig = figure('Visible','off','color','white',...
-    'Position',[300,500,1200,800]);
+    'Units','Normalized','Position',[0 0 1 1]);
 set(compare2fig,'Name','Welcome to Our Awesome Project')
 movegui(compare2fig,'center')
 
-
+%Create back button to go back to opening figure from stat display figures
+back2openfig = uicontrol(singleplayerfig,'Style','pushbutton',...
+    'String','Start Over','Units','Normalized','Position',[0 .95 .05 .05],...
+    'Callback',@back2start);%Callback function that goes back to the opening figure
 
 %Open gui is turned on
 set(openf,'Visible','on')
@@ -67,14 +72,18 @@ set(optionpbg,'Visible','on')
 %Callback function if searching single player
 function spchosen(~,~)
 set(optionpbg,'Visible','off');
+
 %Create button group for single player search 
-oneplayersearch = uibuttongroup('Visible','off','Position',[0 0 .25 1],...
-    'backgroundcolor','white','BorderType','none');
+oneplayersearch = uibuttongroup('Visible','off','Units','Normalized',...
+    'Position',[0 0 .15 1],'backgroundcolor','white','BorderType','none');
+
 %Search for player editable textbox and instruction
-oneplayersearchbox = uicontrol(oneplayersearch,'Style','edit', 'Position', [01 300 200 20], ...
+oneplayersearchbox = uicontrol(oneplayersearch,'Style','edit',...
+    'Units','Normalized','Position',[.05 .35 .8 .05],...
     'Callback', @confirm);%callback function to open fig for single player
 oneplayerinstruct = uicontrol(oneplayersearch,'Style','text',...
-    'Position',[01 350 200 20],'String','Enter player''s last name');
+    'Units','Normalized','Position',[.05 .45 .8 .05],...
+    'String','Enter player''s last name:');
 
 %Make search for player visible
 set(oneplayersearch,'Visible','on');
@@ -135,23 +144,24 @@ function tpchosen(~,~)
 set(optionpbg,'Visible','off')
 %Create button groups for two player search;one to get the first player and
 %next to get the second player
-twoplayersearch_a = uibuttongroup('Visible','off','Position',[0 0 .25 1],...
-    'backgroundcolor','white','BorderType','none');
-twoplayersearch_b = uibuttongroup('Visible','off','Position',[0 0 .25 1],...
-    'backgroundcolor','white','BorderType','none');
+twoplayersearch_a = uibuttongroup('Visible','off','Units','Normalized',...
+    'Position',[0 0 .15 1],'backgroundcolor','white','BorderType','none');
+twoplayersearch_b = uibuttongroup('Visible','off','Units','Normalized',...
+    'Position',[0 0 .15 1],'backgroundcolor','white','BorderType','none');
 
 %Search for player one editable textbox and instruction
 twoplayersearchbox1 = uicontrol(twoplayersearch_a,'Style','edit',...
-    'Position',[01 300 200 20],'Callback',@confirm);
-twoplayersearchbox2 = uicontrol(twoplayersearch_b,'Style', 'edit', ...
-    'Position', [01 300 200 20], 'Callback', @confirm);
-set(twoplayersearchbox1,'Position',[01 300 200 20]);
+    'Units','Normalized','Position',[.05 .35 .8 .05],'Callback',@confirm);
+twoplayersearchbox2 = uicontrol(twoplayersearch_b,'Style','edit',...
+            'Units','Normalized','Position',[.05 .33 .8 .05],...
+            'Callback', @confirm);
 compare2playersinstruct1 = uicontrol(twoplayersearch_a,'Style','text',...
-    'Position',[01 350 200 20],'String','Enter player one''s last name');
+    'Units','Normalized','Position',[.05 .45 .8 .05],...
+    'String','Enter player one''s last name:');
 %Search for player two instruction
 compare2playersinstruct2 = uicontrol(twoplayersearch_b,'Style','text',...
-    'Position',[01 350 200 20],'String','Enter second player''s last name');
-
+    'Units','Normalized','Position',[.05 .4 .8 .05],...
+    'String','Enter second player''s last name:');
 %Make search for player 1 visible
 set(twoplayersearch_a,'Visible','on');
 
@@ -194,7 +204,6 @@ set(twoplayersearch_a,'Visible','on');
                      compare2playersinstruct1.String = 'Did you mean:';
                      twoplayersearchbox1.Visible = 'off';
                      didyoumean = uicontrol(twoplayersearch_a, 'Style', 'popupmenu', 'Position', [01 300 200 20],'String', namearr, 'Callback', @getplayer2);
-                     disp(didyoumean.Value)
                  else
                      compare2playersinstruct2.String = 'Did you mean:';
                      twoplayersearchbox2.Visible = 'off';
@@ -204,17 +213,16 @@ set(twoplayersearch_a,'Visible','on');
         end
 
     
-    function getplayer2 (hObject,~) %Cbfn to print first player's name and prompt for 2nd player FIX THIS SHIT
-        currentplayer1 = players(fullplayerindex(hObject.Value));
-        set(twoplayersearch_a,'Visible','off');
-        player1display = uicontrol(twoplayersearch_b,'Style','text',...
-            'String',currentplayer1.fullname,'Position',[01 400 200 20]); %FIX THIS LINE
-        twoplayersearch_b.Visible = 'on';
-%         twoplayersearchbox2 = uicontrol(twoplayersearch_b,'Style','edit',...
-%             'Position',[01 300 200 20],'Callback',@confirm);
-    end
+        function getplayer2 (hObject,~) %Cbfn to print first player's name and prompt for 2nd player FIX THIS SHIT
+            currentplayer1 = players(fullplayerindex(hObject.Value));
+            set(twoplayersearch_a,'Visible','off');
+            player1display = uicontrol(twoplayersearch_b,'Style','text',...
+                'String',currentplayer1.fullname,'Units','Normalized',...
+                'Position',[.05 .5 .8 .05]); %FIX THIS LINE
+            twoplayersearch_b.Visible = 'on';
+        end
         
-        function opencomp2playerfig(hObject,~)%Cbfn to open 2 player comparison fig
+        function opencomp2playerfig(hObject,~) %Cbfn to open 2 player comparison fig
             currentplayer2 = players(fullplayerindex(hObject.Value));
             set(openf,'Visible','off')
             comptitle = sprintf('%s vs. %s Comparison',currentplayer1.fullname,currentplayer2.fullname);
@@ -223,4 +231,8 @@ set(twoplayersearch_a,'Visible','on');
         end
     end
 end
+    function back2start (~,~)
+        set([singleplayerfig compare2fig],'Visible','off')
+        set([openf optionpbg],'Visible','on')
+    end
 end
